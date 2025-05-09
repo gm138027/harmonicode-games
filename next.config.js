a5d5661562452267225214ef23e38f76d3bd0237
gm138027 <zgm13827@gmail.com>
2025-05-09 11:47:15 +0800

    优化图片加载

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  output: 'export',
+  images: {
+    unoptimized: true, // 禁用图像优化
+  },
+  assetPrefix: './', // 添加这一行，使用相对路径加载资源
 }
 
 module.exports = nextConfig

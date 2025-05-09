92b2d5ba2740478dbd036a9418c9eb3cf38107ba
gm138027 <zgm13827@gmail.com>
2025-05-09 12:56:03 +0800

    修复图片路径问题

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
-   /** @type {import('next').NextConfig} */
-   const nextConfig = {
-     eslint: {
-       ignoreDuringBuilds: true,
-     },
-     output: 'export',
-     images: {
-       unoptimized: true,
-     },
-     assetPrefix: '.',  // 注意这里使用点号而不是'./'
-     trailingSlash: true // 添加这一行以确保URL末尾有斜杠
-   }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: '',
+  basePath: '',
+}
 
-   module.exports = nextConfig
+module.exports = nextConfig
